1847020ce0426bf24d10cf91ac65dd1cbd737a34
ChandrakumarV <chandrakumar252004@gmail.com>
2025-10-07 00:16:18 +0530

    array

---
+++ 10array/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Method 1 arr declaration ----------
+	var arr [3]int
+	fmt.Println(arr)
+	arr[0] = 100
+	arr[1] = 200
+	arr[2] = 300
+	fmt.Println("Numbers are arr : ", arr)
+	fmt.Println("Lenght of No : ", len(arr))
+
+	// Method 2 arr declaration ----------
+	var fruits = []string{"apple", "orange", "mango", "graphs"}
+	fmt.Println(fruits)
+
+}
