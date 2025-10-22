d2c529aeb747eb426fc5de01af89a9005d072744
ChandrakumarV <chandrakumar252004@gmail.com>
2025-10-22 22:39:58 +0530

    LC: update contain dup

--- problems/2. Contains Duplicate/main.go
+++ problems/2. Contains Duplicate/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func containsDuplicate(nums []int) bool {
+func containsDuplicateHashMap(nums []int) bool {
 	m := make(map[int]bool)
 
 	for _, num := range nums {
@@ -14,10 +14,25 @@ func containsDuplicate(nums []int) bool {
 	return false
 }
 
+func containsDuplicateHashSet(nums []int) bool {
+	s := make(map[int]struct{})
+
+	for _, num := range nums {
+		if _, found := s[num]; found {
+			return true
+		}
+		s[num] = struct{}{}
+	}
+	return false
+}
+
 func main() {
 
 	arr := []int{3, 6, 4, 2, 7, 8, 2, 11}
 
-	result := containsDuplicate(arr)
-	fmt.Println(result)
+	resultHashMap := containsDuplicateHashMap(arr)
+	resultHashSet := containsDuplicateHashSet(arr)
+
+	fmt.Println("Hash Map:", resultHashMap)
+	fmt.Println("Hash Set:", resultHashSet)
 }
